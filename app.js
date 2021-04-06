47df5842bb99671f686889ccc5f777041cbfab37
Kalinin Dmitry <vega-kim@mail.ru>
2021-04-06 10:57:25 +0300

    error handling with middleware

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const AppError = require('./utils/appError');
 const morgan = require('morgan');
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
+const errorControler = require('./controllers/errorControler');
 
 const app = express();
 
@@ -16,5 +18,12 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
+
+app.all('*', (req, res, next) => {
+  const err = new AppError(`Can\'t find ${req.originalUrl}`, 404);
+  next(err);
+});
+
+app.use(errorControler);
 
 module.exports = app;
